be14a2bca50561a4123384b51e9aa2308b080847
Andres Carballo Nercellas <Andres@MacBook-de-Andres.local>
2014-03-04 22:53:06 +0100

    Add generation snapshot from console

--- src/jfcc/Config.java
+++ src/jfcc/Config.java
@@ -1,6 +1,6 @@
 package jfcc;
 
 public class Config {
-	static String VERSION = "0.1.a1";
-	static String BUILD = "20140303-0001";
+	static String VERSION = "0.1.a2";
+	static String BUILD = "20140304-0002";
 }
--- src/jfcc/Console.java
+++ src/jfcc/Console.java
@@ -1,18 +1,24 @@
 package jfcc;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Console extends JFrame implements WindowListener{
+public class Console extends JFrame implements WindowListener, ActionListener{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private JButton oSearchFolder = null;
 
 	public Console(){
 		this.addWindowListener(this);
@@ -26,10 +32,41 @@ public class Console extends JFrame implements WindowListener{
 	private JPanel getMainPanel(){
 		JPanel oJPanel = new JPanel();
 		
-		JLabel olabel = new JLabel("Only command version: jfcc.jar [folder] [folder]");
+		JLabel olabel = new JLabel("Command version: jfcc.jar [folder] [folder]");
+		
 		oJPanel.add(olabel);
+		oJPanel.add(getSearchFolder());
 		
 		return oJPanel;
+	}
+	
+	private JButton getSearchFolder(){
+		if(this.oSearchFolder == null){
+			this.oSearchFolder = new JButton();
+			this.oSearchFolder.setName("oSearchFolder");
+			this.oSearchFolder.setText("Search");
+			this.oSearchFolder.addActionListener(this);	
+		}
+		return this.oSearchFolder;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getSource().equals(getSearchFolder())){
+			JFileChooser oChooser = new JFileChooser();
+			oChooser.setDialogTitle("Select directory");
+			oChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int returnVal = oChooser.showOpenDialog(this);
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				String sPath = oChooser.getSelectedFile().getAbsolutePath();
+				Snapshot oSnapshot = new Snapshot(sPath);
+				oSnapshot.addFileIgnored(".svn");
+				oSnapshot.loadSnapshot();
+				oSnapshot.refreshSnapshotCRC32();
+				//oSnapshot.show();
+				oSnapshot.export();
+			}
+		}
 	}
 	
 	@Override
